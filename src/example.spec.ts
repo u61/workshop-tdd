8f653da415cdb0436cce4c18a4fa12d08ab969e7
u61 <utkukopuz.nl@outlook.com>
2020-09-17 22:10:10 +0200

    write tests via tdd

--- src/example.spec.ts
+++ src/example.spec.ts
@@ -1,7 +1,46 @@
 // See: https://jestjs.io/docs/en/api for the api of Jest
 
-describe('example spec', () => {
-  it('knows things', () => {
-    expect(14*3).toEqual(42);
+function calculateGrossPerItem(unitPricePerItem, quantity) {
+  return unitPricePerItem * quantity;
+}
+
+function calculateTotalGross(totalGrossValues) {
+  return totalGrossValues.reduce((a, b) => a + b);
+}
+
+function calculateNetValue(grossValue) {
+  if (grossValue > 200) {
+    return (grossValue * 90) / 100;
+  } else if (grossValue > 100) {
+    return (grossValue * 95) / 100;
+  } else {
+    return grossValue;
+  }
+}
+
+describe('shopping cart', () => {
+  it('total gross value of one element will be calculated as per total quantity and unit price.', () => {
+    const unitPricePerItem = 10.0;
+    const quantity = 5;
+
+    const totalGrossPerItem = calculateGrossPerItem(unitPricePerItem, quantity);
+
+    expect(totalGrossPerItem).toEqual(50);
+  });
+
+  it('total gross value of shopping cart will be calculated as sum of values of all items', () => {
+    const totalGrossValues = [50, 51, 104];
+
+    const totalGrossPerItem = calculateTotalGross(totalGrossValues);
+
+    expect(totalGrossPerItem).toEqual(205);
+  });
+
+  it('if total gross value higher than €100, find net value as %5 less', () => {
+    const grossValue = 100;
+
+    const netValue = calculateNetValue(grossValue);
+
+    expect(netValue).toEqual(95);
   });
 });
